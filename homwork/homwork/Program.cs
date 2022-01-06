ed890dc3e9b6148692c58264b57606202232cc7a
Boyan A <80381466+BoyanAleksandrov@users.noreply.github.com>
2022-01-06 15:52:59 +0200

    Add files via upload

---
+++ homwork/homwork/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace homwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            var count = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (count.ContainsKey(number))
+                {
+                    count[number]++;
+                }
+                else {
+                    count[number] = 1;
+                }
+
+
+            }
+
+            foreach (var number in count)
+            {
+
+                Console.WriteLine(number.Key + " -> " + number.Value);
+
+            }
+
+        }
+    }
+}
